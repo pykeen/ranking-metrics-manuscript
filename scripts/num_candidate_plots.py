cf485713f85579396761ee1959fd1f8381ebcc48
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-14 20:19:51 +0100

    Update candidate plot

--- scripts/num_candidate_plots.py
+++ scripts/num_candidate_plots.py
@@ -16,54 +16,25 @@ sns.set_style("white")
 
 def main():
     num_samples = 500  # any bigger is unnecessary
-    num_candidates = np.logspace(2, 7).reshape(-1, 1)
+    num_candidates = np.logspace(np.log10(2), 7, num=60).reshape(-1, 1)
     inverse_metrics = [
         InverseHarmonicMeanRank(),
         InverseArithmeticMeanRank(),
         InverseGeometricMeanRank(),
         HitsAtK(10),
     ]
-    inverse_expectations = pd.DataFrame(
-        [
-            (metric.__class__.__name__, x[0].item(), metric.expected_value(x, num_samples))
-            for metric in inverse_metrics
-            for x in num_candidates
-        ],
-        columns=["metric", "x", "y"]
-    )
-
-    inverse_variances = pd.DataFrame(
-        [
-            (metric.__class__.__name__, x[0].item(), metric.variance(x, num_samples))
-            for metric in inverse_metrics
-            for x in num_candidates
-        ],
-        columns=["metric", "x", "y"]
-    )
-
     metrics = [
         HarmonicMeanRank(),
         ArithmeticMeanRank(),
         GeometricMeanRank(),
     ]
-    num_candidates = np.logspace(1, 6).reshape(-1, 1)
-    expectations = pd.DataFrame(
-        [
-            (metric.__class__.__name__, x[0].item(), metric.expected_value(x, num_samples))
-            for metric in metrics
-            for x in num_candidates
-        ],
-        columns=["metric", "x", "y"]
+    inverse_expectations, inverse_variances = _get_dfs(
+        inverse_metrics, num_candidates=num_candidates, num_samples=num_samples,
+    )
+    expectations, variances = _get_dfs(
+        metrics, num_candidates=num_candidates, num_samples=num_samples,
     )
 
-    variances = pd.DataFrame(
-        [
-            (metric.__class__.__name__, x[0].item(), metric.variance(x, num_samples))
-            for metric in metrics
-            for x in num_candidates
-        ],
-        columns=["metric", "x", "y"]
-    )
     fig, ((lax, rax), (lax_inv, rax_inv)) = plt.subplots(2, 2, figsize=(10, 7))
 
     sns.lineplot(data=expectations, x="x", y="y", hue="metric", ax=lax)
@@ -90,6 +61,26 @@ def main():
     fig.savefig(CHARTS_DIRECTORY.joinpath("candidate_plot.svg"))
     fig.savefig(CHARTS_DIRECTORY.joinpath("candidate_plot.png"), dpi=300)
     fig.savefig(CHARTS_DIRECTORY.joinpath("candidate_plot.pdf"))
+
+
+def _get_dfs(metrics, num_candidates, num_samples):
+    expectations = pd.DataFrame(
+        [
+            (metric.__class__.__name__, x[0].item(), metric.expected_value(x, num_samples))
+            for metric in metrics
+            for x in num_candidates
+        ],
+        columns=["metric", "x", "y"]
+    )
+    variances = pd.DataFrame(
+        [
+            (metric.__class__.__name__, x[0].item(), metric.variance(x, num_samples))
+            for metric in metrics
+            for x in num_candidates
+        ],
+        columns=["metric", "x", "y"]
+    )
+    return expectations, variances
 
 
 if __name__ == '__main__':
